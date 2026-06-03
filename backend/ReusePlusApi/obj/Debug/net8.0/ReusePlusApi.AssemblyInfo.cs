8b184d0177ac4897a9b7c6413f5098d9b594224f
MathiasFilypeDev <mathiasfilypedev@gmail.com>
2026-06-03 18:10:37 -0300

    Ajustes iniciais

--- backend/ReusePlusApi/obj/Debug/net8.0/ReusePlusApi.AssemblyInfo.cs
+++ backend/ReusePlusApi/obj/Debug/net8.0/ReusePlusApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReusePlusApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e032f7ede6151fa332932dd8afaa065a8799bc90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d906c0d794bc3cbb5bea7c17c1e4db8649dcae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReusePlusApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReusePlusApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
