d3063040ae24735b4f59d9292c74de046b31ded0
MathiasFilypeDev <mathiasfilypedev@gmail.com>
2026-06-14 17:05:53 -0300

    Ajustando o login do admin

--- backend/ReusePlusApi/obj/Debug/net8.0/ReusePlusApi.AssemblyInfo.cs
+++ backend/ReusePlusApi/obj/Debug/net8.0/ReusePlusApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReusePlusApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12871528cefb6f6a1a299b7165759043a17af4c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae6b80d429b32102a948240b266f0ab0a9476ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReusePlusApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReusePlusApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
